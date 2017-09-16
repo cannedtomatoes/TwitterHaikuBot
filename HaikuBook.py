52033b813b5e0062f96e34a4bfabfefd1e930b88
Andrew Ardill <andrew.ardill@gmail.com>
2017-09-16 21:50:23 +1000

    Use syllables_en.count() for syllable count

--- HaikuBook.py
+++ HaikuBook.py
@@ -1,44 +1,12 @@
 import re
 import sys
 from random import sample
-import random 
+import random
+import syllables_en
 
 #Function to count syllables
 def CountSyllables(word, isName=True):
-    vowels = "aeiouy"
-    #single syllables in words like bread and lead, but split in names like Breanne and Adreann
-    specials = ["ia","ea"] if isName else ["ia"]
-    specials_except_end = ["ie","ya","es","ed"]  #seperate syllables unless ending the word
-    currentWord = word.lower()
-    numVowels = 0
-    lastWasVowel = False
-    last_letter = ""
-
-    for letter in currentWord:
-        if letter in vowels:
-            #don't count diphthongs unless special cases
-            combo = last_letter+letter
-            if lastWasVowel and combo not in specials and combo not in specials_except_end:
-                lastWasVowel = True
-            else:
-                numVowels += 1
-                lastWasVowel = True
-        else:
-            lastWasVowel = False
-
-        last_letter = letter
-
-    #remove es & ed which are usually silent
-    if len(currentWord) > 2 and currentWord[-2:] in specials_except_end:
-        numVowels -= 1
-
-    #remove silent single e, but not ee since it counted it before and we should be correct
-    elif len(currentWord) > 2 and currentWord[-1:] == "e" and currentWord[-2:] != "ee":
-        numVowels -= 1
-
-    return numVowels
-#End function    
-#-------------------------
+	return syllables_en.count(word)
 
 # Start main program
 attempts = 4000 # attempts at finding sentence with given word
