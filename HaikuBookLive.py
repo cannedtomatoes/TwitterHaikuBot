04fe537f8d0b0b9e392a4825eaf24baa2ce6b8a5
Chris Pocock <31438031+cannedtomatoes@users.noreply.github.com>
2017-09-23 18:47:25 +1000

    Update HaikuBookLive.py

--- HaikuBookLive.py
+++ HaikuBookLive.py
@@ -166,10 +166,10 @@ final.capitalize()
 print("Posting:\n", final, "\n")
 
 #Define our constant variables, this is all the data we wrote down in the first part of the tutorial.
-CONSUMER_KEY = 	'wjcxNTp8xeyprkpDLg6sxi2k4'
-CONSUMER_SECRET = 'QFup2rdBBfvNqhDInul7KLcAOAR83QCTjN1HokuqFI3ZrQrGJu'
-ACCESS_KEY = '907828033503076354-4FGZZAi9rWwVnjIYPGUJcKmbObpVqba'
-ACCESS_SECRET = 'sYsxpiteUc12kyQnYbWNl91xvGk71m3jFdQVeWySfF2Uv'
+CONSUMER_KEY = 	
+CONSUMER_SECRET = 
+ACCESS_KEY = 
+ACCESS_SECRET = 
 
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
 api.update_status(status = final)
